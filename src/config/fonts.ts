09657e382da2a1dda4daba716775e15af15ab904
jojupacr5 <joseph.paucar@gmail.com>
2024-08-26 21:46:21 -0500

    First commit

---
+++ src/config/fonts.ts
@@ -0,0 +1,7 @@
+import { Nunito, Limelight } from "next/font/google";
+
+export const nunito = Nunito({ subsets: ["latin"] });
+export const titleFont = Limelight({
+  subsets: ["latin"],
+  weight: ["400"],
+});
